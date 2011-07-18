2e3e9db474f7aaa45139db2a1d63602e2fa05acc
Jonne Nauha <jonne.nauha@adminotech.com>
2011-07-19 02:08:42 +0300

    Visual Studio 2008 prebuild build_deps

---
+++ build_deps/kNet/include/kNetBuildConfig.h
@@ -0,0 +1,21 @@
+// This file is programmatically generated using CMake.
+// This file shows the compilation settings that were used to build kNet.
+// These need to match for the client code using kNet.
+#pragma once
+
+#ifndef KNET_ENABLE_WINXP_SUPPORT
+#define KNET_ENABLE_WINXP_SUPPORT
+#endif
+
+#ifndef KNET_LOGGING_SUPPORT_ENABLED
+#define KNET_LOGGING_SUPPORT_ENABLED
+#endif
+
+#ifndef KNET_USE_BOOST
+#define KNET_USE_BOOST
+#endif
+
+#ifndef KNET_USE_TINYXML
+#define KNET_USE_TINYXML
+#endif
+
---
+++ build_deps/libmumbleclient/lib/readme.txt
@@ -0,0 +1,1 @@
+Builded from sources:http://github.com/mattiku/libmumbleclient : commit  208da05bbf009f776b1b (2010-11-15)
