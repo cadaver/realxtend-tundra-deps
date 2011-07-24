8a8d8c88f4287ebc0aa53e73bf84a45321b16cee
Jonne Nauha <jonne.nauha@adminotech.com>
2011-07-25 02:32:04 +0300

    VC2010 build_deps.

---
+++ build_deps/kNet/include/kNetBuildConfig.h
@@ -0,0 +1,25 @@
+// This file is programmatically generated using CMake.
+// This file shows the compilation settings that were used to build kNet.
+// These need to match for the client code using kNet.
+#pragma once
+
+#ifndef KNET_ENABLE_WINXP_SUPPORT
+#define KNET_ENABLE_WINXP_SUPPORT
+#endif
+
+#ifndef KNET_THREAD_CHECKING_ENABLED
+#define KNET_THREAD_CHECKING_ENABLED
+#endif
+
+#ifndef KNET_LOGGING_SUPPORT_ENABLED
+#define KNET_LOGGING_SUPPORT_ENABLED
+#endif
+
+#ifndef KNET_NETWORK_PROFILING
+#define KNET_NETWORK_PROFILING
+#endif
+
+#ifndef KNET_USE_BOOST
+#define KNET_USE_BOOST
+#endif
+
---
+++ build_deps/libmumbleclient/include/mumbleclient/client_lib.h
@@ -0,0 +1,35 @@
+#ifndef CLIENT_LIB_H_
+#define CLIENT_LIB_H_
+
+#include "visibility.h"
+#include "libmumble_stdint.h"
+
+#include <boost/asio.hpp>
+
+namespace MumbleClient {
+
+class MumbleClient;
+
+class DLL_PUBLIC MumbleClientLib {
+  public:
+	static MumbleClientLib* instance();
+	MumbleClient* NewClient();
+	void Run();
+	void Shutdown();
+	static int32_t GetLogLevel();
+	static void SetLogLevel(int32_t level);
+
+  private:
+	DLL_LOCAL MumbleClientLib();
+	DLL_LOCAL ~MumbleClientLib();
+
+	DLL_LOCAL static MumbleClientLib* instance_;
+	boost::asio::io_service io_service_;
+
+	MumbleClientLib(const MumbleClientLib&);
+	void operator=(const MumbleClientLib&);
+};
+
+}  // namespace MumbleClient
+
+#endif  // CLIENT_LIB_H_
