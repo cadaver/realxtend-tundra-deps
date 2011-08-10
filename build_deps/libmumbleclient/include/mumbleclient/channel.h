c940a989ddfc0fb441a0af6e2f9aea4e0834563d
Jonne Nauha <jonne.nauha@evocativi.com>
2011-08-10 07:06:44 +0300

    Mumble deps: libcelt 0.11.1, google protobuf and libmumbleclient (latest from migus repo).

--- build_deps/libmumbleclient/include/mumbleclient/channel.h
+++ build_deps/libmumbleclient/include/mumbleclient/channel.h
@@ -1,27 +1,26 @@
-#ifndef CHANNEL_H_
-#define CHANNEL_H_
+#ifndef _LIBMUMBLECLIENT_CHANNEL_H_
+#define _LIBMUMBLECLIENT_CHANNEL_H_
 
 #include <boost/weak_ptr.hpp>
-
 #include "logging.h"
 
 namespace MumbleClient {
 
 class Channel {
-  public:
-	Channel(int32_t id_) : id(id_) { }
-	~Channel() { DLOG(INFO) << "Channel " << name << " destroyed"; }
-	int32_t id;
-	boost::weak_ptr<Channel> parent;
-	int32_t position;
-	bool temporary;
-	std::string name;
-	std::string description;
-//	std::vector<Channel> links;
+public:
+    Channel(int32_t id_) : id(id_) { }
+    ~Channel() { DLOG(INFO) << "Channel " << name << " destroyed"; }
+    int32_t id;
+    boost::weak_ptr<Channel> parent;
+    int32_t position;
+    bool temporary;
+    std::string name;
+    std::string description;
+    //	std::vector<Channel> links;
 
-  private:
-	Channel(const Channel&);
-	void operator=(const Channel&);
+private:
+    Channel(const Channel&);
+    void operator=(const Channel&);
 };
 
 }  // namespace MumbleClient
--- build_deps/libmumbleclient/include/mumbleclient/client_lib.h
+++ build_deps/libmumbleclient/include/mumbleclient/client_lib.h
@@ -1,34 +1,33 @@
-#ifndef CLIENT_LIB_H_
-#define CLIENT_LIB_H_
-
-#include <stdint.h>
-
-#include <boost/asio.hpp>
+#ifndef _LIBMUMBLECLIENT_CLIENT_LIB_H_
+#define _LIBMUMBLECLIENT_CLIENT_LIB_H_
 
 #include "visibility.h"
+#include "libmumble_stdint.h"
+
+#include <boost/asio.hpp>
 
 namespace MumbleClient {
 
 class MumbleClient;
 
 class DLL_PUBLIC MumbleClientLib {
-  public:
-	static MumbleClientLib* instance();
-	MumbleClient* NewClient();
-	void Run();
-	void Shutdown();
-	static int32_t GetLogLevel();
-	static void SetLogLevel(int32_t level);
+public:
+    static MumbleClientLib* instance();
+    MumbleClient* NewClient();
+    void Run();
+    void Shutdown();
+    static int32_t GetLogLevel();
+    static void SetLogLevel(int32_t level);
 
-  private:
-	DLL_LOCAL MumbleClientLib();
-	DLL_LOCAL ~MumbleClientLib();
+private:
+    DLL_LOCAL MumbleClientLib();
+    DLL_LOCAL ~MumbleClientLib();
 
-	DLL_LOCAL static MumbleClientLib* instance_;
-	boost::asio::io_service io_service_;
+    DLL_LOCAL static MumbleClientLib* instance_;
+    boost::asio::io_service io_service_;
 
-	MumbleClientLib(const MumbleClientLib&);
-	void operator=(const MumbleClientLib&);
+    MumbleClientLib(const MumbleClientLib&);
+    void operator=(const MumbleClientLib&);
 };
 
 }  // namespace MumbleClient
